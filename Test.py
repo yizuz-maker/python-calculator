64f795898f82bf0e9acc8eb99f2a005666d8525d
Jesus <jesuscambronero2019@gmail.com>
2023-05-05 09:11:56 -0300

    Suma y Resta
    Se trabajó sobre las funciones de resta y suma

--- Test.py
+++ Test.py
@@ -11,8 +11,23 @@ def subtraction(valor1, valor2):
 print("Python Calculator")
 print("------------------")
 print("1------------Suma")
-print("2------------Resta")
+print("2------------Resta \n")
 
-ask = int(input("Selecione una opcion"))
-valor1 = int(input("Ingrese valor 1: "))
-valor2 = int(input("Ingrese valor 2: "))
+ask = int(input("Selecione una opcion: "))
+
+match ask:
+    case 1:
+        print("-----Suma-----")
+        valor1 = int(input("Ingrese valor 1: "))
+        valor2 = int(input("Ingrese valor 2: "))
+        out = addition(valor1, valor2)
+
+        print(f"El resultado es {out}")
+
+    case 2:
+        print("-----Resta-----")
+        valor1 = int(input("Ingrese valor 1: "))
+        valor2 = int(input("Ingrese valor 2: "))
+        out = subtraction(valor1, valor2)
+
+        print(f"El resultado es {out}")
