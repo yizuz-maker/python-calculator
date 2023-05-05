959dfd2a366abf673cca6dabc9b0beba1f300bd6
Jesus <jesuscambronero2019@gmail.com>
2023-05-05 08:49:30 -0300

    Se agregó el archivo.py a Develop
    El archivo no estaba en el área de trabajo, se agregó

---
+++ Test.py
@@ -0,0 +1,18 @@
+def addition(valor1, valor2):
+    suma = valor1 + valor2
+    return suma
+
+
+def subtraction(valor1, valor2):
+    resta = valor1 + valor2
+    return resta
+
+
+print("Python Calculator")
+print("------------------")
+print("1------------Suma")
+print("2------------Resta")
+
+ask = int(input("Selecione una opcion"))
+valor1 = int(input("Ingrese valor 1: "))
+valor2 = int(input("Ingrese valor 2: "))
