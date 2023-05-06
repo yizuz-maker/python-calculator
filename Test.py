d8456b76d8f9bfa7a958f9ab50b28d49f2621482
Jesus-Desktop <jesusofftopic@gmail.com>
2023-05-06 15:46:58 -0300

    input_valores, os.system("cls") y message()
    Se creó una funcion para el ingreso de valores. Se agregó una funcionalidad para limpiar la pantalla. Se agregó el mensaje principal a una funcion

--- Test.py
+++ Test.py
@@ -1,28 +1,37 @@
-from os import system
+import os
 
 ask = ""
 valor1 = 0
 valor2 = 0
 out = 0
 
-# La funcion toma valores depurados
-
 
 def message():
-    system.cls()
+    os.system("cls")
     print("Python Calculator")
     print("------------------")
     print("1------------Suma")
     print("2------------Resta \n")
 
 
-def addition(valor1, valor2):
+def input_valores():
+    try:
+        return float(input("Ingrese valor 1: ")),  float(input("Ingrese valor 2: "))
+    except ValueError:
+        print("Ingrese un número")
+        return input_valores()
+
+
+def addition(element1):
+
+    valor1 = element1[0]
+    valor2 = element1[1]
     suma = valor1 + valor2
     return suma
 
 
-def subtraction(valor1, valor2):
-    resta = valor1 + valor2
+def subtraction(element1):
+    resta = valor1 - valor2
     return resta
 
 
@@ -31,35 +40,33 @@ message()
 while True:
     try:
         ask = int(input("Selecione una opcion: "))
-
         if ask > 2 or ask < 1:
             raise Exception
-
         else:
             break
+
     except ValueError:
         print("Ingrese un número")
 
     except Exception:
-        print(
-            f"No se encontró la opcion {ask}, ingrese una válida")
+        print(f"No se encontró la opcion {ask}, ingrese una válida")
 
 
 match ask:
     case 1:
-        print("\n")
+        os.system("cls")
         print("-----Suma-----")
-        valor1 = int(input("Ingrese valor 1: "))
-        valor2 = int(input("Ingrese valor 2: "))
-        out = addition(valor1, valor2)
+
+        valores = input_valores()
+        out = addition(valores)
 
         print(f"El resultado es {out}")
 
     case 2:
-        print("\n")
+        os.system("cls")
         print("-----Resta-----")
-        valor1 = int(input("Ingrese valor 1: "))
-        valor2 = int(input("Ingrese valor 2: "))
-        out = subtraction(valor1, valor2)
+
+        valores = input_valores()
+        out = subtraction(valores)
 
         print(f"El resultado es {out}")
